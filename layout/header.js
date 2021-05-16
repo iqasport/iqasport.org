ee5380ddd9c9669bdae700335c309489637b0556
Declan Ramsay <declandr@gmail.com>
2021-05-16 17:43:53 +0100

    Add logo to navigation and link to home

--- layout/header.js
+++ layout/header.js
@@ -1,13 +1,23 @@
-import { Flex, Heading } from 'components';
+import {
+  Flex,
+  Image,
+  HStack,
+  useBreakpointValue,
+  Link as ChakraLink,
+} from 'components';
+import Link from 'next/link';
 import LanguageSwitcher from 'components/language-switcher';
 
 export default function Header({ page /* lang */ }) {
+  const logoHeight = useBreakpointValue({ base: 40, xl: 75 }) || 40;
+  const logoTextHeight = useBreakpointValue({ base: 40, xl: 70 }) || 40;
+  const logoTextWidth = useBreakpointValue({ base: 100, xl: 160 }) || 100;
   return (
     <Flex
       justifyContent="space-between"
       alignItems="center"
       as="header"
-      h="60px"
+      h="100px"
       bg="white"
       position="sticky"
       top="0"
@@ -15,7 +25,29 @@ export default function Header({ page /* lang */ }) {
       zIndex={10}
       px={3}
     >
-      <Heading color="iqaGreen">IQA</Heading>
+      <Link href="/" passHref>
+        <ChakraLink>
+          <HStack spacing={2}>
+            <Image
+              src="/images/logo.png"
+              alt="International Quidditch Association"
+              priority={true}
+              layout="fixed"
+              height={logoHeight}
+              width={logoHeight}
+            />
+            <Image
+              src="/images/logo-text.png"
+              alt="International Quidditch Association"
+              priority={true}
+              layout="fixed"
+              height={logoTextHeight}
+              width={logoTextWidth}
+            />
+          </HStack>
+        </ChakraLink>
+      </Link>
+
       <LanguageSwitcher altLangs={page?.alternate_languages} />
     </Flex>
   );
