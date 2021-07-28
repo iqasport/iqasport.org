9888d41fcfcb2195ddcbc1159f058a9f52a5f5be
Declan Ramsay <declandr@gmail.com>
2021-07-28 23:49:46 +0100

    Add IQA favicons

--- document/head.js
+++ document/head.js
@@ -8,23 +8,28 @@ export default function DocumentHead() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/favicon/favicon-16x16.png"
       />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#bb0a1e" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#62b058"
+      />
+      <meta name="msapplication-TileColor" content="#00a300" />
+      <meta name="theme-color" content="#62b058" />
 
       <meta
         property="og:site_name"
