9beb4d6a6ef15357a9f27b09f09acbcdda68b3db
Declan Ramsay <declan.ramsay@starlingbank.com>
2021-08-27 10:59:29 +0100

    Improve member cards slice fetching each card individually

--- components/prismic/member-cards.tsx
+++ components/prismic/member-cards.tsx
@@ -10,32 +10,84 @@ import { useEffect, useState } from 'react';
 
 const MIN_CARDS = 3;
 
-const MemberCardsSlice = (rawData) => {
-  const [members, setMembers] = useState([]);
+const MemberCard = ({ member, variant }) => {
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    if (!data) {
+      const getMember = async () => {
+        const { data } = await getPrismicDocByUid('members', member.uid);
+
+        setData(data);
+      };
+
+      getMember();
+    }
+  }, [data, setData, member.uid]);
+
+  const linkProps = {
+    href: Link.url(data?.link, linkResolver),
+    ariaLabel: data?.title,
+    target: '_blank',
+    rel: 'noopener noreferrer',
+  };
+
+  const content = (
+    <>
+      <Heading fontSize="3xl" fontFamily="body" mb={0} mt={0}>
+        {data?.title}
+      </Heading>
+      <Text fontSize="sm" fontStyle="italic" mt={2}>
+        <InfoOutlineIcon mr={2} color="gray.400" />
+        {data?.country}
+        <br />
+        <ExternalLinkIcon mr={2} color="gray.400" />
+        {data?.link?.url}
+      </Text>
+    </>
+  );
+
+  if (!data) {
+    return (
+      <Flex flexDirection="column">
+        <Card />
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex flexDirection="column">
+      <Card
+        {...linkProps}
+        content={content}
+        variant={cardVariants[variant]}
+        image={{
+          src: data?.ngb_photo?.url,
+          alt: data?.ngb_photo?.alt,
+          height: data?.ngb_photo?.dimensions?.height,
+          width: data?.ngb_photo?.dimensions?.width,
+        }}
+        logo={{
+          src: data?.ngb_logo?.url,
+          alt: data?.ngb_logo?.alt,
+          height: data?.ngb_logo?.dimensions?.height,
+          width: data?.ngb_logo?.dimensions?.width,
+        }}
+      />
+    </Flex>
+  );
+};
+
+const MemberCardsSlice = (rawData) => {
   const title = get(rawData, 'primary.title');
   const content = get(rawData, 'primary.content');
   const variant = get(rawData, 'primary.variant');
-
-  useEffect(() => {
-    const items = get(rawData, 'items') ?? [];
-
-    const getMembers = async () => {
-      const membersPromises = items.map(({ member }) =>
-        getPrismicDocByUid('members', member.uid)
-      );
-
-      const settled = await Promise.all(membersPromises);
-      setMembers(settled);
-    };
-
-    getMembers();
-  }, [rawData, setMembers]);
+  const items = get(rawData, 'items');
 
   let spacers = [];
 
-  if (members.length < MIN_CARDS) {
-    spacers = new Array(MIN_CARDS - members.length).fill(0);
+  if (items.length < MIN_CARDS) {
+    spacers = new Array(MIN_CARDS - items.length).fill(0);
   }
 
   return (
@@ -62,57 +114,15 @@ const MemberCardsSlice = (rawData) => {
         gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))"
         gridGap={8}
       >
-        {members.map(({ data }, i) => {
-          const linkProps = {
-            href: Link.url(data?.link, linkResolver),
-            ariaLabel: data?.title,
-            target: '_blank',
-            rel: 'noopener noreferrer',
-          };
-
-          const content = (
-            <>
-              <Heading fontSize="3xl" fontFamily="body" mb={0} mt={0}>
-                {data?.title}
-              </Heading>
-              <Text fontSize="sm" fontStyle="italic" mt={2}>
-                <InfoOutlineIcon mr={2} color="gray.400" />
-                {data?.country}
-                <br />
-                <ExternalLinkIcon mr={2} color="gray.400" />
-                {data?.link?.url}
-              </Text>
-            </>
-          );
-
-          return (
-            <Flex
-              flexDirection="column"
-              key={`cards-${i}-${data?.title}-${data?.content}`}
-            >
-              <Card
-                {...linkProps}
-                // title={data?.title}
-                content={content}
-                variant={cardVariants[variant]}
-                image={{
-                  src: data?.ngb_photo?.url,
-                  alt: data?.ngb_photo?.alt,
-                  height: data?.ngb_photo?.dimensions?.height,
-                  width: data?.ngb_photo?.dimensions?.width,
-                }}
-                logo={{
-                  src: data?.ngb_logo?.url,
-                  alt: data?.ngb_logo?.alt,
-                  height: data?.ngb_logo?.dimensions?.height,
-                  width: data?.ngb_logo?.dimensions?.width,
-                }}
-              />
-            </Flex>
-          );
-        })}
+        {items.map(({ member }, i) => (
+          <MemberCard
+            variant={variant}
+            member={member}
+            key={`member-card-${i}-${member?.uid}`}
+          />
+        ))}
         {!!spacers.length &&
-          spacers.map((space, i) => <div key={`spacer-${i}`} />)}
+          spacers.map((_, i) => <div key={`spacer-member-cards-${i}`} />)}
       </Grid>
     </Slice>
   );
