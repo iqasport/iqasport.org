26a7f3c050b7ca244a3ba82fbf1f8cf842845d9f
Declan Ramsay <declandr@gmail.com>
2021-10-03 16:20:34 +0100

    Dynamic import prismic slices

--- modules/prismic.tsx
+++ modules/prismic.tsx
@@ -1,21 +1,29 @@
 import Prismic from '@prismicio/client';
-import {
-  Hero,
-  HeaderAndParagraph,
-  Images,
-  ImageAndContent,
-  Cards,
-  Embed,
-  EmbedAndContent,
-  Banner,
-  HorizontalCards,
-  LatestNews,
-  Carousel,
-  MemberCards,
-  ContactForm,
-  WorldMap,
-  NewsCards,
-} from 'components/prismic';
+import dynamic from 'next/dynamic';
+
+const Hero = dynamic(() => import('components/prismic/hero'));
+const HeaderAndParagraph = dynamic(
+  () => import('components/prismic/header-and-paragraph')
+);
+const Images = dynamic(() => import('components/prismic/images'));
+const ImageAndContent = dynamic(
+  () => import('components/prismic/image-and-content')
+);
+const Cards = dynamic(() => import('components/prismic/cards'));
+const Embed = dynamic(() => import('components/prismic/embed'));
+const EmbedAndContent = dynamic(
+  () => import('components/prismic/embed-and-content')
+);
+const HorizontalCards = dynamic(
+  () => import('components/prismic/horizontal-cards')
+);
+const LatestNews = dynamic(() => import('components/prismic/latest-news'));
+const Banner = dynamic(() => import('components/prismic/banner'));
+const Carousel = dynamic(() => import('components/prismic/carousel'));
+const MemberCards = dynamic(() => import('components/prismic/member-cards'));
+const ContactForm = dynamic(() => import('components/prismic/contact-form'));
+const WorldMap = dynamic(() => import('components/prismic/world-map'));
+const NewsCards = dynamic(() => import('components/prismic/news-cards'));
 
 const REPOSITORY = process.env.NEXT_PUBLIC_PRISMIC_REPOSITORY_NAME;
 const REF_API_URL = `https://${REPOSITORY}.prismic.io/api/v2`;
