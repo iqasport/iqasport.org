7c34281f259ef426ece2de94ed91a62221d3d31d
Declan Ramsay <declandr@gmail.com>
2021-10-17 17:26:56 +0100

    Remove emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,7 @@ import type { AppProps } from 'next/app';
 import GTag, { pageview } from 'modules/analytics';
 import dynamic from 'next/dynamic';
 import { ChakraProvider } from '@chakra-ui/react';
-import { EmotionCache } from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
 import theme from 'styles/theme';
-import createEmotionCache from 'styles/createEmotionCache';
 
 const Layout = dynamic(() => import('layout'));
 const Fonts = dynamic(() => import('styles/fonts'));
@@ -16,17 +13,7 @@ const AppErrorBoundary = dynamic(
   () => import('components/errorBoundaries/app')
 );
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-const clientSideEmotionCache = createEmotionCache();
-
-function MyApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}: MyAppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
@@ -45,13 +32,11 @@ function MyApp({
       <GTag />
       <DocumentHead />
       <Fonts />
-      <CacheProvider value={emotionCache}>
-        <ChakraProvider theme={theme} resetCSS={false}>
-          <Layout {...pageProps}>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
-      </CacheProvider>
+      <ChakraProvider theme={theme} resetCSS={false}>
+        <Layout {...pageProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
     </AppErrorBoundary>
   );
 }
