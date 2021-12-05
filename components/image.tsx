e18537c5ca012b8f8275c1df0849427bb4d6dc97
Declan Ramsay <declandr@gmail.com>
2021-12-05 23:29:00 +0000

    Fix types issue with builds for borderRadius and clipPath

--- components/image.tsx
+++ components/image.tsx
@@ -1,9 +1,16 @@
 import NextImage from 'next/image';
 import { Box } from '@chakra-ui/react';
 
-const ImageWithDefaults = ({ alt, src, borderRadius, clipPath, ...props }) => {
+const ImageWithDefaults = ({ alt, src, ...props }) => {
   return (
-    <Box sx={{ '& img': { borderRadius: borderRadius ?? '2xl', clipPath } }}>
+    <Box
+      sx={{
+        '& img': {
+          borderRadius: props?.borderRadius ?? '2xl',
+          clipPath: props?.clipPath ?? 'initial',
+        },
+      }}
+    >
       <NextImage
         src={src}
         alt={alt}
