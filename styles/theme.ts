1d6cf6a45f4ea2803b40c22cdc25108469d5804e
Marian Dziubiak <marian.dziubiak@gmail.com>
2023-06-21 22:36:33 +0100

    Fix revert change to style after downgrading chakra

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { SliceStyles } from 'components/slice';
 import { CardStyles } from 'components/card';
 import { HorizontalCardStyles } from 'components/horizontal-card';
@@ -8,12 +9,12 @@ const emBase = 16;
 export const rem = (value: number) => `${value / emBase}rem`;
 
 export default extendTheme({
-  breakpoints: {
+  breakpoints: createBreakpoints({
     sm: rem(576),
     md: rem(768),
     lg: rem(992),
     xl: rem(1200),
-  },
+  }),
 
   colors: {
     iqaGreen: '#62b058',
